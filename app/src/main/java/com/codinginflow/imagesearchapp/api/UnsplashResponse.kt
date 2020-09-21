863c253b687161bbabdec56988d97a5ccb836f8b
Novruz Jafarov <novruz.jafarov@azerimed.com>
2020-09-21 15:31:00 +0400

    api service created

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/api/UnsplashResponse.kt
@@ -0,0 +1,8 @@
+package com.codinginflow.imagesearchapp.api
+
+import com.codinginflow.imagesearchapp.model.UnsplashPhoto
+
+data class UnsplashResponse(
+    val resutls: List<UnsplashPhoto>,
+
+)
---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,12 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+
+
+
+
+}
